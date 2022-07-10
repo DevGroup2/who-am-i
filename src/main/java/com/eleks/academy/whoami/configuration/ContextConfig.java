f90e443810bbafce3a3065ba06ca91aa8037d9a0
TarasDzivik <t.dzivik@gmail.com>
2022-07-10 13:06:50 +0300

    Deleted @EnableWebMvc

--- src/main/java/com/eleks/academy/whoami/configuration/ContextConfig.java
+++ src/main/java/com/eleks/academy/whoami/configuration/ContextConfig.java
@@ -5,14 +5,10 @@ import com.eleks.academy.whoami.networking.server.ServerImpl;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.io.IOException;
 
 @Configuration
-@EnableWebMvc
 @EnableConfigurationProperties(ServerProperties.class)
 public class ContextConfig {
 
